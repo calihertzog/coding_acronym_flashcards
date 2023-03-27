0062f48673fd8f27227c9c774c7bdf9ecdce1983
calihertzog <127066402+calihertzog@users.noreply.github.com>
2023-03-26 20:19:05 -0700

    add box and knowledge check functionality

--- cards/urls.py
+++ cards/urls.py
@@ -5,15 +5,19 @@ from . import views
 
 urlpatterns = [
     path("",
-        views.CardListView.as_view(), 
-        name="card-list"
+         views.CardListView.as_view(), 
+         name="card-list"
     ),
     path("new/",
-        views.CardCreateView.as_view(),
-        name="card-create"
+         views.CardCreateView.as_view(),
+         name="card-create"
     ),
     path("edit/<int:pk>",
          views.CardUpdateView.as_view(),
          name="card-update"
+    ),
+    path("box/<int:box_num>",
+         views.BoxView.as_view(),
+         name="box"
     )
 ]
